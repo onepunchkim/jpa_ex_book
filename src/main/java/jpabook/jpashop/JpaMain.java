c008bfc731b16ac3579215d506b9a5364375826a
onepunchkim <hyukjin4711@gmail.com>
2025-09-07 18:05:13 +0900

    jpa_ex Inheritance mapping

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 
 
 public class JpaMain {
@@ -18,7 +19,11 @@ public class JpaMain {
 
         try {
 
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("kim");
 
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
